cd7a644dd48e644f1feee0eea5421c014322eb58
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-06-30 22:41:13 +0300

    Finish basic LoginModel

--- Models/LoginModel.php
+++ Models/LoginModel.php
@@ -3,8 +3,17 @@
 
 class LoginModel extends Model
 {
-    public function __construct()
+    public function __construct($db)
     {
-        echo "Login model created";
+        parent::__construct($db);
+        $this->main_table = "users";
+    }
+
+    public function can_user_login($login){
+        $sth = $this->db->select_where($this->main_table, ['password'], ['login' => $login]); //TODO check login for injections
+        if (count($sth) > 2)
+            throw new DatabaseException("More than one entry ", 10);
+        else if (count($sth) < 1 || !hash_equals($sth[0]['password'], hash('whirlpool', $_POST['passwd'])))
+            throw new WrongCredentialsException();
     }
 }
