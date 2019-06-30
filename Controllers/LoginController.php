1d99f4f9d2055a33de75c67b3f04318933741353
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-06-30 22:38:45 +0300

    Finish basic functionality of LoginController: Login, logout and default;

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -6,9 +6,38 @@ class LoginController extends Controller
     public function actionDefault($params = false)
     {
         // TODO: Implement actionDefault() method.
+        if ($this->loggedIn) {
+            header("Location: " . URL); //TODO redirect to previous location
+            exit;
+        }
         $this->view->render('login', array('title' => 'Login'));
     }
-    public function login(){
-        $this->model->get_single();
+
+    public function actionLogin(){
+        if (!isset($_POST['login']) || !isset($_POST['passwd'])){
+            header("Location: " . URL); //TODO redirect to previous location
+            exit;
+        }
+        try {
+            $this->model->can_user_login($_POST['login']);
+        } catch (DatabaseException $e) {
+            if ($e->getCode() == 10)
+                ErrorController::action500();
+            else
+            {
+                //TODO render correspond message (in future may be make it through ajax?)
+                header("Location: " . URL.'login');
+            }
+            die;
+        }
+        Session::init();
+        Session::set('loggedIn', true);
+        header("Location: ".URL); //TODO redirect to previous location
+        exit;
+    }
+
+    public function actionLogout(){
+        Session::destroy();
+        header("Location: ".URL); //TODO redirect to previous location
     }
 }
