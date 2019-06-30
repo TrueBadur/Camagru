d7db9deb77104e5a1e1d33b7ea2fcf625718167b
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-06-30 22:37:30 +0300

    Make ErrorController static to be able to call it from anywhere

--- Controllers/ErrorController.php
+++ Controllers/ErrorController.php
@@ -1,21 +1,21 @@
 <?php
 
-class ErrorController extends Controller
+class ErrorController
 {
-    function action404(){
+    static $view;
+
+    static function action404($msg) {
         header("HTTP/1.1 404 Not Found");
-        $this->view->render('error', array('error_msg' => '404 Page not found', 'title' => '404 Not found'));
+        echo $msg;
+        $view = new View();
+        $view->render('error', array('error_msg' => '404 Page not found', 'title' => '404 Not found'));
         exit;
     }
 
-    function action500(){
+    static function action500() {
         header("HTTP/1.1 500 Internal Error");
-        $this->view->render('error', array('error_msg' => '500 Internal Error', 'title' => '500 Internal error'));
+        $view = new View();
+        $view->render('error', array('error_msg' => '500 Internal Error', 'title' => '500 Internal error'));
         exit;
-    }
-
-    public function actionDefault($params = false)
-    {
-        self::action404();
     }
 }
